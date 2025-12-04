105bdea22690b071b59a9d95c0c246c14d79cdf7
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2025-12-04 14:40:03 +0530

    Product Item code updated with dispatch and links

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,21 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 
 export default function ProductItem({ product }) {
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    dispatch(addToCart(product)); // add item to cart
+  };
+
   return (
-    <div className="product-card">
-      {/* Lazy loading for images */}
-      <img
-        src={product.thumbnail}
-        alt={product.title}
-        loading="lazy"
-      />
+    <div className="product-card" role="article">
+      <img src={product.thumbnail} alt={product.title} loading="lazy" /> {/* product image */}
+      <h3>{product.title}</h3> {/* product title */}
+      <p>₹ {product.price}</p> {/* product price */}
 
-      <h3>{product.title}</h3>
-      <p>₹ {product.price}</p>
+      <div className="actions"> {/* buttons row */}
+        <Link to={`/product/${product.id}`}>
+          <button className="ghost" type="button">View Details</button> {/* open details page */}
+        </Link>
 
-      <Link to={`/product/${product.id}`}>
-        <button>View Details</button>
-      </Link>
+        <button onClick={handleAdd} type="button">Add to Cart</button> {/* add to cart */}
+      </div>
     </div>
   );
 }
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, // id type
+    title: PropTypes.string.isRequired, // product title
+    price: PropTypes.number.isRequired, // product price
+    thumbnail: PropTypes.string // image url
+  }).isRequired
+};
